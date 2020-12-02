91586d234735142c55addb149b0996c398dc97c0
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-12-02 23:57:16 +0100

    one step back screens code

--- components/Screen2.js
+++ components/Screen2.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text} from 'react-native';
 
-export default class Screen2 extends react.Component {
+
+export default class Screen2 extends React.Component {
   render() {
-    let name = this.props.route.params.name; // OR ...
-    // let { name } = this.props.route.params;
-    this.props.navigation.setOptions({ title: name });
-
     return (
       <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        {/* Rest of the UI */}
-        <Text>Chat Screen2 </Text>
+        <Text>Hello Screen2!</Text>
       </View>
     )
   }
