6e0a277f8792e9d01f65df5e4ad269d76c03b2b8
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-12-02 23:20:11 +0100

    import dependencies

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 // import the screens
 import Screen1 from './components/Screen1';
 import Screen2 from './components/Screen2';
+// import react native gesture handler
+import 'react-native-gesture-handler';
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
