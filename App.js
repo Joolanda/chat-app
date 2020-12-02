aa4e85b352da0654f7daacfe37a6caa2794d7498
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-12-02 23:14:33 +0100

    Start with clean App.js

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Chat World!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
