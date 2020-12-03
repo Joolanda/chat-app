d646d28b330f823e13a154b877c9e043e2183ebc
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-12-03 22:34:04 +0100

    using design specs for styling

--- components/Start.js
+++ components/Start.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 // The application’s Start component that renders the openings-screen UI export default class Start extends Component {...
 export default class Start extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: '' };
+  }
   render() {
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello Screen1!</Text>
+      <View style={styles.container}>
+        <Text style={{fontSize:45, fontWeight: '600', fontColor: '#FFFFFF'}}>Chat App!</Text>
+        <TextInput 
+          style={styles.box2}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+          placeholder='Your Name'
+        />        
         <Button
-          title="Go to Screen 2"
+          title="Start Chatting"
           onPress={() => this.props.navigation.navigate('Chat')}
         />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  box2: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+
+  }
+
+});
