7de68d215065321e493ab18b02f91b2a4e471027
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-12-11 15:00:24 +0100

    adding comments and fixing

--- components/Chat.js
+++ components/Chat.js
@@ -3,18 +3,25 @@ import { View, Text, TextInput, Button} from 'react-native';
 
 // The application’s main Chat component that renders the chat UI export default class Chat extends Component {...
 export default class Chat extends React.Component {
-
+  constructor() {
+    super();
+  }
+ 
   // Initializing state user
   render() {
-    //Defining variable(s) from Start screen
-    let { name }= this.props.route.params;
-    let { colorSelect}= this.props.route.params;
+    //Defining variables from Start screen
+    let { name, colorSelect }= this.props.route.params;
+    // Display user's name in the navbar at the top of the chat screen
     this.props.navigation.setOptions({ title: name });
-    this.props.navigation.setOptions({ backgroundColor: colorSelect });
 
     return (
       <View 
-        style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: colorSelect }}>
+        style={{
+          flex:1, 
+          justifyContent: 'center', 
+          alignItems: 'center', 
+          backgroundColor: colorSelect
+        }}>
         <Text>Hello Screen2!</Text>
       </View>
     )
