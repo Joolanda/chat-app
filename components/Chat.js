a01ef414edf6b89ed8979fd869720145cf231f61
Joolanda <50619917+Joolanda@users.noreply.github.com>
2021-01-14 19:38:45 +0100

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 // Bubble is a third party tool to customize styling of the gifted chat bubble 
 import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 // By importing keyboardAvoidingView you can solve the issue with keyboard position on Android devices
@@ -9,7 +9,7 @@ const firebase = require('firebase');
 require('firebase/firestore');
 // The application’s main Chat component that renders the chat UI export default class Chat extends Component
 
-export default class Chat extends Component {
+export default class Chat extends React.Component {
   // Initializing the state in order to send, receive and display messages
   
   constructor() {
@@ -155,7 +155,7 @@ export default class Chat extends Component {
   // Initializing state user
   render() {
     // Defining variables from Start screen
-    let { name, colorSelect }= this.props.route.params;
+    let { name, colorSelect } = this.props.route.params;
     // Set a default username in case the user didn't enter one
     // if (!user || user === '') user = 'User';
     // Display user's name in the navbar at the top of the chat screen
